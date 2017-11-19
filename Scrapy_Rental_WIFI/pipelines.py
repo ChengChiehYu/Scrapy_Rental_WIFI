b06ed184bfb2687bce386bbe9daa343e26103782
Python1991 <chiyu12205@gmail.com>
2017-11-20 00:59:15 +0800

    v0.0.2

--- Scrapy_Rental_WIFI/pipelines.py
+++ Scrapy_Rental_WIFI/pipelines.py
@@ -6,11 +6,12 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 import sqlite3
+from scrapy.exceptions import DropItem
+
 class ScrapyRentalWifiPipeline(object):
     def open_spider(self, spider):
         self.conn = sqlite3.connect('Rental_WIFI.sqlite')
         self.cur =self.conn.cursor()
-        # self.cur.execute('create table if not exists WIFI(title varchar(1000));')
         self.cur.execute('create table if not exists Rental_WIFI(title text,url text,discount_price_program text,discount_price_description text,discount_price_day1_text text,discount_price_day1_price text,discount_price_day1_after_text text,discount_price_day1_after_price text,daily_rental_program text,daily_rental_day1_text text,daily_rental_day1_price text,daily_rental_day1_after_text text,daily_rental_day1_after_price text,monthly_rental_program text,monthly_rental_text1 text,monthly_rental_text2 text,crawl_time text);')
 
     def close_spider(self, spider):
@@ -18,6 +19,9 @@ class ScrapyRentalWifiPipeline(object):
         self.conn.close()
 
     def process_item(self, item, spider):
+        # dic = dict(item)
+        # del dic['crawl_time']
+        # print(dic)
         col = ','.join(item.keys())
         placeholders = ','.join(len(item) * '?')
         sql = 'insert into Rental_WIFI({}) values({})'
@@ -26,14 +30,17 @@ class ScrapyRentalWifiPipeline(object):
             items.append(v)
 
         if(self.is_old_item(items)):
+            DropItem('item exists already')
+        else:
+            self.cur.execute(sql.format(col, placeholders), items)
             return item
-        self.cur.execute(sql.format(col, placeholders), items)
-        return item
 
     def is_old_item(self , items):
         sql = 'select * from Rental_WIFI where url = ({})'
         res = self.cur.execute(sql.format('?'),(items[1],))
         row = self.cur.fetchone()
+        print(items)
+
         if row == None:
             return False
 
