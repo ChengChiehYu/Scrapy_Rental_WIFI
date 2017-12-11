98b09b64423d52d0a16ea8d80f71010ffa676825
Python1991 <chiyu12205@gmail.com>
2017-12-11 22:58:04 +0800

    v0.0.18

---
+++ Scrapy_Rental_WIFI/spiders/JH4.py
@@ -0,0 +1,329 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import requests
+from bs4 import BeautifulSoup
+import time
+import random
+import re
+import datetime
+
+do_ip_check = False
+do_search_around = False
+do_confuse_search = False
+do_send_keys_slow = False
+do_time_sleep = False
+
+f = open('configure.txt', 'r' , encoding = 'UTF-8')
+# 隨機搜尋字
+confusion_words = f.readline().replace('\ufeff','')
+confusion_words = confusion_words.split(',')
+del confusion_words[-1]
+# print(confusion_words)
+
+# 隨機搜尋-修飾字
+global modification_words_of_confusion
+modification_words_of_confusion = f.readline()
+modification_words_of_confusion = modification_words_of_confusion.split(',')
+del modification_words_of_confusion[-1]
+# print(modification_words_of_confusion)
+
+# 保護網址
+protect_words = f.readline()
+protect_words = protect_words.split(',')
+del protect_words[-1]
+#print(protect_words)
+
+# 關鍵字
+keywords = f.readline()
+keywords = keywords.split(',')
+del keywords[-1]
+#print(keywords)
+
+# 關鍵字-修飾字
+modification_words_of_keywords = f.readline()
+modification_words_of_keywords = modification_words_of_keywords.split(',')
+del modification_words_of_keywords[-1]
+# print(modification_words_of_keywords)
+
+# 目標網址
+target_url = f.readline()
+target_url = target_url.split(',')
+del target_url[-1]
+# print(target_url)
+
+# 瀏覽頁面1-9
+surf_page_times = f.readline()
+surf_page_times = surf_page_times.split(',')
+del surf_page_times[-1]
+# print(surf_page_times)
+
+# 頁面停留10-90
+page_sleep_times = f.readline()
+page_sleep_times = page_sleep_times.split(',')
+del page_sleep_times[-1]
+# print(page_sleep_times)
+
+# 執行時間
+exercise_time = f.readline()
+exercise_time = exercise_time.split(',')
+del exercise_time[-1]
+# print(exercise_time)
+
+spt0 = int(surf_page_times[0])
+spt1 = int(surf_page_times[1])
+
+pst0 = int(page_sleep_times[0])
+pst1 = int(page_sleep_times[1])
+
+def click_keywords(driver):
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "_WGk"))
+    )
+    elements = driver.find_elements_by_class_name('_WGk')
+    target_index = list()
+    for ii in range(len(elements)):
+        if(not target_url):
+            if(not is_protect_word(elements[ii].text)):
+                target_index.append(ii)
+        else:
+            if(is_target_url(elements[ii].text)):
+                target_index.append(ii)
+            
+    target2 = '//h3/a[2]'
+    elements2 = driver.find_elements_by_xpath(target2)
+    r_num = random.choice(target_index)
+    if(do_time_sleep):
+        time.sleep(2)
+    print(elements[r_num].text)
+    elements2[r_num].click()
+    return search_around(driver, random.randint(spt0,spt1),"")
+
+def is_exercise_time():
+    hour = datetime.datetime.now().hour
+    start = int(exercise_time[0])
+    end = int(exercise_time[1])
+    if(start > end):
+        if(hour >= start or hour < end):
+            return True
+        else:
+            return False
+    else:
+        if(hour >= start and hour < end):
+            return True
+        else:
+            return False
+
+def is_protect_word(text):
+    for protect_word in protect_words:
+        if(re.search(protect_word, text)):
+            return True
+    return False
+
+def is_target_url(text):
+    for url in target_url:
+        if(re.search(url, text)):
+            return True
+    return False
+
+def search_around(driver ,times, string):
+    if(not do_search_around):
+        times = 0
+    ind = 0
+    for i in range(times):
+        try:
+            if(do_time_sleep):
+                time.sleep(random.randint(pst0,pst1))            
+            elements = driver.find_elements_by_tag_name('a')
+            if(len(elements) == 0):
+                ind = ind - 1
+                driver.back()
+            rand_num = random.randint(0,len(elements)-1)
+            elements[rand_num].click()
+            ind = ind + 1
+#             print(i)
+        except Exception as e:
+#             print(e)
+            pass
+    return ind
+    
+def confuse_search(driver, times, times2, scrpit):
+    if(not do_confuse_search):
+        times = 0
+    for i in range(times):    
+        driver.execute_script(script); 
+        driver.get('https://www.google.com.tw')
+        inputElement = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.ID, "lst-ib"))
+        )
+        global modification_words_of_confusion
+        modification_words_of_confusion = list(modification_words_of_confusion)
+        prefix = ''
+        if(random.randint(0,1)):
+            prefix = random.choice(modification_words_of_confusion)
+            modification_words_of_confusion.remove(prefix)
+        suffix = ''
+        if(random.randint(0,1)):
+            suffix = random.choice(modification_words_of_confusion)  
+        confusion_word = random.choice(confusion_words)
+        confusion_word = prefix + confusion_word + suffix
+        send_keys_slow(inputElement, confusion_word)
+        inputElement.submit()
+        if(do_time_sleep):
+            time.sleep(1)
+        try:
+            print('confuse-start')
+            for ii in range(10):
+                try:
+                    target2 = '//h3/a[1]'
+                    elements = driver.find_elements_by_xpath(target2)
+                    random.choice(elements).click()
+                    search_around(driver ,times2,'')
+                    break;
+                except:
+                    pass
+            print('confuse-end')
+        except Exception as e:
+#             print(e)
+            pass
+def send_keys_slow(inputElement, string):
+    for i in range(len(string)):
+        inputElement.send_keys(string[i])
+        if(do_send_keys_slow):
+            time.sleep(random.randint(30,150)/100)
+def get_current_ip():
+    res = requests.get('http://ipinfo.io')
+    soup = BeautifulSoup(res.text,'lxml')
+    ip = soup.select('p')[0]
+    res = re.search("([0-9]{1,3}\.){3}[0-9]{1,3}", ip.text)
+    return res.group()
+
+def get_random_country_geolocation():
+    country_list = {
+        '新北市':(121.6739,24.91571),
+        '高雄市':(120.666,23.01087),
+        '臺中市':(120.9417,24.23321),
+        '臺北市':(121.5598,25.09108),
+        '桃園縣':(121.2168,24.93759),
+        '臺南市':(120.2513,23.1417),
+        '彰化縣':(120.4818,23.99297),
+        '屏東縣':(120.62,22.54951),
+        '雲林縣':(120.3897,23.75585),
+        '苗栗縣':(120.9417,24.48927),
+        '嘉義縣':(120.574,23.45889),
+        '新竹縣':(121.1252,24.70328),
+        '南投縣':(120.9876,23.83876),
+        '宜蘭縣':(121.7195,24.69295),
+        '新竹市':(120.9647,24.80395),
+        '基隆市':(121.7081,25.10898),
+        '花蓮縣':(121.3542,23.7569),
+        '嘉義市':(120.4473,23.47545),
+        '臺東縣':(120.9876,22.98461),
+#         '金門縣':(118.3186,24.43679),
+#         '澎湖縣':(119.6151,23.56548),
+#         '連江縣':(119.5397,26.19737),
+    }
+    country = random.choice(list(country_list.keys()))
+    geolocation = country_list[country]
+    return country,geolocation[0], geolocation[1]
+
+# cmp_ip = '61.216.99.1'
+cmp_ip = '114.41.253.213'
+origin_ip = cmp_ip
+
+index2 = 0
+while True:
+    if(not is_exercise_time()):
+        time.sleep(60)
+    index2 = index2 +1
+    while True:
+        try:
+            if(not do_ip_check):
+                break
+            new_ip = get_current_ip()        
+#             print(origin_ip)
+            if(origin_ip != new_ip and cmp_ip != new_ip):
+                origin_ip = new_ip
+                print(str(index2)+'------index')
+                print(new_ip)
+                break
+            time.sleep(30)
+        except Exception as e:
+#             print(e)
+            pass
+    try:
+        country, latitude, longitude= get_random_country_geolocation()
+        print(country)
+        lf = (random.randint(0,1000)-500)/1000000
+        tb = (random.randint(0,1000)-500)/1000000
+        script = '''
+          window.navigator.geolocation.getCurrentPosition = function(success) {
+            var position = { coords : { latitude: "%s", longitude: "%s" } }; 
+            success(position);
+          }
+        ''' % (latitude+lf, longitude+tb)
+        driver = webdriver.Chrome()
+        driver.delete_all_cookies()
+        
+
+        driver.switch_to_window('')
+        driver.maximize_window()
+
+        confuse_search(driver, random.randint(0,1), random.randint(spt0,spt1), script)
+        confuse_search(driver, random.randint(0,1), random.randint(spt0,spt1), script)
+
+        
+        driver.execute_script(script); 
+        driver.get('https://www.google.com.tw')
+        inputElement = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.ID, "lst-ib"))
+        )
+        modification_words_of_keywords = list(modification_words_of_keywords)
+        prefix = ''
+        if(random.randint(0,1)):
+            prefix = random.choice(modification_words_of_keywords)
+            modification_words_of_keywords.remove(prefix)
+        suffix = ''
+        if(random.randint(0,1)):
+            suffix = random.choice(modification_words_of_keywords)        
+        
+        keyword = random.choice(keywords)
+        keyword = prefix + keyword + suffix
+        send_keys_slow(inputElement, keyword)
+        driver.execute_script(script); 
+
+        inputElement.submit()
+        driver.execute_script(script);
+#         time.sleep(3)
+#         driver.refresh()
+#         element = driver.find_element_by_xpath('//*[@id="nav"]/tbody/tr/td[3]/a')
+#         element.click()
+#         time.sleep(10)
+        try:
+            ind = click_keywords(driver)
+            if(random.randint(0,1)):
+                for j in range(ind):
+                    try:
+                        driver.back()
+                        elements = driver.find_elements_by_class_name('_WGk')
+                        click_keywords(driver)
+                        break
+                    except Exception as e:
+                        print(e,'--c')
+                        
+        except Exception as e:
+            print(e,'--a')
+            pass
+            
+        confuse_search(driver, random.randint(0,1),random.randint(spt0,spt1), script)
+        driver.delete_all_cookies()
+    except Exception as e:
+        print(e,'--b')
+        pass
+    finally:
+        driver.quit()
+
+
+    
+
--- Scrapy_Rental_WIFI/spiders/interface.py
+++ Scrapy_Rental_WIFI/spiders/interface.py
@@ -1,34 +1,84 @@
 # f = open('configure.txt', 'w' , encoding = 'UTF-8')
 
+# # 隨機搜尋字
+# # 隨機搜尋-修飾字
+# # 保護網址
+# # 關鍵字
+# # 關鍵字-修飾字
+# # 瀏覽頁面1-9
+# # 頁面停留10-90
+# # 目標網址
+# # 執行時間
 # f.write('抓姦推薦,徵信,抓小三,跟監,抓猴,通姦,通姦罪,通姦犯法嗎,性交,徵信社,跟拍,跟拍多少錢,通姦的定義,通姦案例,配偶與人通姦,第三者,外遇,一夜情,通姦除罪,老公外遇,老公偷吃,老公怪怪的,老婆怪怪的,外遇蒐證,婚前徵信,婚姻恐懼症,尋人,尋人啟事,工商徵信,應收帳款,文書鑑定,鑑定文書,諮詢法律問題,法律問題,公益活動,一統徵信厲害嗎,一統,一桶徵信,一統徵信,徵信必破,徵信價格,婚外情,外遇調查,抓姦訴訟求償,抓姦求償,抓姦訴訟,訴訟求償,婚前,身家調查,家暴蒐證,個人行蹤,離婚專業,反跟蹤,\n')
+# f.write('推薦,廣告,\n')
 # f.write('www.twgoodspy.com,www.tw07.biz,www.wanqing.org,www.topspy.org,\n')
-# f.write('推薦,廣告,抓姦,抓奸,好評,厲害,第一,最強,最快,徵信,台北,ptt,first,bset,recommend,\n')
 # f.write('徵信社收費,徵信社費用,徵信社價錢,\n')
+# f.write('推薦,廣告,抓姦,抓奸,好評,厲害,第一,最強,最快,徵信,台北,ptt,first,recommend,\n')
+# f.write('\n')
+# f.write('1,2,\n')
+# f.write('10,90,\n')
+# f.write('0,24,\n')
 # f.close()
 
 import tkinter as tk
 import time
 
 f = open('configure.txt', 'r' , encoding = 'UTF-8')
+# 隨機搜尋字
 confusion_words = f.readline().replace('\ufeff','')
 confusion_words = confusion_words.split(',')
 del confusion_words[-1]
 #print(confusion_words)
 
+# 隨機搜尋-修飾字
+modification_words_of_confusion = f.readline()
+modification_words_of_confusion = modification_words_of_confusion.split(',')
+del modification_words_of_confusion[-1]
+#print(modification_words_of_confusion)
+
+# 保護網址
 protect_words = f.readline()
 protect_words = protect_words.split(',')
 del protect_words[-1]
 #print(protect_words)
 
-modification_words = f.readline()
-modification_words = modification_words.split(',')
-del modification_words[-1]
-#print(modification_words)
-
+# 關鍵字
 keywords = f.readline()
 keywords = keywords.split(',')
 del keywords[-1]
-#print(modification_words)
+#print(keywords)
+
+# 關鍵字-修飾字
+modification_words_of_keywords = f.readline()
+modification_words_of_keywords = modification_words_of_keywords.split(',')
+del modification_words_of_keywords[-1]
+# print(modification_words_of_keywords)
+
+# 目標網址
+target_url = f.readline()
+target_url = target_url.split(',')
+del target_url[-1]
+# print(target_url)
+
+# 瀏覽頁面1-9
+surf_page_times = f.readline()
+surf_page_times = surf_page_times.split(',')
+del surf_page_times[-1]
+# print(surf_page_times)
+
+# 頁面停留10-90
+page_sleep_times = f.readline()
+page_sleep_times = page_sleep_times.split(',')
+del page_sleep_times[-1]
+# print(page_sleep_times)
+
+# 執行時間
+exercise_time = f.readline()
+exercise_time = exercise_time.split(',')
+del exercise_time[-1]
+# print(exercise_time)
+
+
 f.close()
 
 def exit():
@@ -40,6 +90,11 @@ def save_and_exit():
     f.write(var2_2.get()[1:-1].replace("'" , '').replace(" " , '')+',\n')
     f.write(var3_2.get()[1:-1].replace("'" , '').replace(" " , '')+',\n')
     f.write(var4_2.get()[1:-1].replace("'" , '').replace(" " , '')+',\n')
+    f.write(var5_2.get()[1:-1].replace("'" , '').replace(" " , '')+',\n')
+    f.write(var6_2.get()[1:-1].replace("'" , '').replace(" " , '')+',\n')
+    f.write(en7_1.get()+','+en7_2.get()+',\n')
+    f.write(en8_1.get()+','+en8_2.get()+',\n')
+    f.write(en9_1.get()+','+en9_2.get()+',\n')
     f.close()
     exit()
 
@@ -47,7 +102,7 @@ def add_keyword(en,lb):
     if(en.get() !=''):
         lb.insert(0,en.get())
         en.delete(0,tk.END)
-        print(var1_2.get())
+#         print(var1_2.get())
         
 def delete_keyword(lb):
     select_keyword = lb.curselection()
@@ -57,11 +112,15 @@ def delete_keyword(lb):
         
 window = tk.Tk()
 window.title('my window')
-window.geometry('450x750+650+100')
+window.geometry('1100x750+650+100')
 padxy = 10
 
+#frame left
+frl = tk.Frame(window)
+frl.grid(row=0,column=0)
+
 # frame1
-fr1 = tk.Frame(window)
+fr1 = tk.Frame(frl)
 fr1.grid(row=0,column=0,padx=padxy,pady=padxy)
 
 var1_1 = tk.StringVar()
@@ -87,11 +146,11 @@ lb1.config(yscrollcommand=sc1.set)
 sc1.config(command=lb1.yview)
 
 # frame2
-fr2 = tk.Frame(window)
+fr2 = tk.Frame(frl)
 fr2.grid(row=0,column=1,padx=padxy,pady=padxy)
 
 var2_1 = tk.StringVar()
-var2_1.set('保護網址')
+var2_1.set('隨機搜尋-修飾字')
 lab2_1 = tk.Label(fr2, textvariable = var2_1)
 lab2_1.grid(row=0,column=0,columnspan=2,padx=padxy,pady=padxy)
 
@@ -104,7 +163,7 @@ btn2_2 = tk.Button(fr2, text = '刪除',command = lambda : delete_keyword(lb2))
 btn2_2.grid(row=2,column=1,padx=padxy,pady=padxy)
 
 var2_2 = tk.StringVar()
-var2_2.set(protect_words)
+var2_2.set(modification_words_of_confusion)
 lb2 = tk.Listbox(fr2,listvariable=var2_2)
 lb2.grid(row=3,column=0,columnspan=3,padx=padxy,pady=padxy)
 sc2 = tk.Scrollbar(fr2, orient="vertical")
@@ -113,11 +172,11 @@ lb2.config(yscrollcommand=sc2.set)
 sc2.config(command=lb2.yview)
 
 # frame3
-fr3 = tk.Frame(window)
-fr3.grid(row=1,column=0,padx=padxy,pady=padxy)
+fr3 = tk.Frame(frl)
+fr3.grid(row=0,column=2,padx=padxy,pady=padxy)
 
 var3_1 = tk.StringVar()
-var3_1.set('修飾字')
+var3_1.set('保護網址')
 lab3_1 = tk.Label(fr3, textvariable = var3_1)
 lab3_1.grid(row=0,column=0,columnspan=2,padx=padxy,pady=padxy)
 
@@ -130,7 +189,7 @@ btn3_2 = tk.Button(fr3, text = '刪除',command = lambda : delete_keyword(lb3))
 btn3_2.grid(row=2,column=1,padx=padxy,pady=padxy)
 
 var3_2 = tk.StringVar()
-var3_2.set(modification_words)
+var3_2.set(protect_words)
 lb3 = tk.Listbox(fr3,listvariable=var3_2)
 lb3.grid(row=3,column=0,columnspan=3,padx=padxy,pady=padxy)
 sc3 = tk.Scrollbar(fr3, orient="vertical")
@@ -139,8 +198,8 @@ lb3.config(yscrollcommand=sc3.set)
 sc3.config(command=lb3.yview)
 
 # frame4
-fr4 = tk.Frame(window)
-fr4.grid(row=1,column=1,padx=padxy,pady=padxy)
+fr4 = tk.Frame(frl)
+fr4.grid(row=1,column=0,padx=padxy,pady=padxy)
 
 var4_1 = tk.StringVar()
 var4_1.set('關鍵字')
@@ -165,13 +224,135 @@ lb4.config(yscrollcommand=sc4.set)
 sc4.config(command=lb4.yview)
 
 #frame 5
+fr5 = tk.Frame(frl)
+fr5.grid(row=1,column=1,padx=padxy,pady=padxy)
 
-fr_5 = tk.Frame(window)
-fr_5.grid(row=2,column=0,columnspan=2,padx=padxy,pady=padxy)
-btn5_1 = tk.Button(fr_5, text = '保存並退出',command = save_and_exit)
-btn5_1.grid(row=0,column=0,padx=padxy,pady=padxy)
-btn5_2 = tk.Button(fr_5, text = '不儲存離開',command = exit)
-btn5_2.grid(row=0,column=1,padx=padxy,pady=padxy)
+var5_1 = tk.StringVar()
+var5_1.set('關鍵字-修飾字')
+lab5_1 = tk.Label(fr5, textvariable = var5_1)
+lab5_1.grid(row=0,column=0,columnspan=2,padx=padxy,pady=padxy)
+
+en5 = tk.Entry(fr5)
+en5.grid(row=1,column=0,columnspan=2,padx=padxy,pady=padxy)
+
+btn5_1 = tk.Button(fr5, text = '增加',command = lambda : add_keyword(en5,lb5))
+btn5_1.grid(row=2,column=0,padx=padxy,pady=padxy)
+btn5_2 = tk.Button(fr5, text = '刪除',command = lambda : delete_keyword(lb5))
+btn5_2.grid(row=2,column=1,padx=padxy,pady=padxy)
+
+var5_2 = tk.StringVar()
+var5_2.set(modification_words_of_keywords)
+lb5 = tk.Listbox(fr5,listvariable=var5_2)
+lb5.grid(row=3,column=0,columnspan=3,padx=padxy,pady=padxy)
+sc5 = tk.Scrollbar(fr5, orient="vertical")
+sc5.grid(row=3,column=3,padx=padxy,pady=padxy)
+lb5.config(yscrollcommand=sc5.set)
+sc5.config(command=lb4.yview)
+
+#frame 6
+fr6 = tk.Frame(frl)
+fr6.grid(row=1,column=2,padx=padxy,pady=padxy)
+
+var6_1 = tk.StringVar()
+var6_1.set('目標網址')
+lab6_1 = tk.Label(fr6, textvariable = var6_1)
+lab6_1.grid(row=0,column=0,columnspan=2,padx=padxy,pady=padxy)
+
+en6 = tk.Entry(fr6)
+en6.grid(row=1,column=0,columnspan=2,padx=padxy,pady=padxy)
+
+btn6_1 = tk.Button(fr6, text = '增加',command = lambda : add_keyword(en6,lb6))
+btn6_1.grid(row=2,column=0,padx=padxy,pady=padxy)
+btn6_2 = tk.Button(fr6, text = '刪除',command = lambda : delete_keyword(lb6))
+btn6_2.grid(row=2,column=1,padx=padxy,pady=padxy)
+
+var6_2 = tk.StringVar()
+var6_2.set(target_url)
+lb6 = tk.Listbox(fr6,listvariable=var6_2)
+lb6.grid(row=3,column=0,columnspan=3,padx=padxy,pady=padxy)
+sc6 = tk.Scrollbar(fr6, orient="vertical")
+sc6.grid(row=3,column=3,padx=padxy,pady=padxy)
+lb6.config(yscrollcommand=sc6.set)
+sc6.config(command=lb4.yview)
+
+#frame right
+frr = tk.Frame(window)
+frr.grid(row=0,column=1)
+
+#frame 7
+fr7 = tk.Frame(frr)
+fr7.grid(row=0,column=0,padx=padxy,pady=padxy)
+
+var7_1 = tk.StringVar()
+var7_1.set('瀏覽頁面次數')
+lab7_1 = tk.Label(fr7, textvariable = var7_1)
+lab7_1.grid(row=0,column=0,columnspan=3,padx=padxy,pady=padxy)
+
+en7_1 = tk.Entry(fr7,width=5)
+en7_1.insert(0,surf_page_times[0])
+en7_1.grid(row=1,column=0,padx=padxy,pady=padxy)
+
+var7_2 = tk.StringVar()
+var7_2.set('-')
+lab7_2 = tk.Label(fr7, textvariable = var7_2)
+lab7_2.grid(row=1,column=1,padx=padxy,pady=padxy)
+
+en7_2 = tk.Entry(fr7,width=5)
+en7_2.insert(0,surf_page_times[1])
+en7_2.grid(row=1,column=2,padx=padxy,pady=padxy)
+
+#frame 8
+fr8 = tk.Frame(frr)
+fr8.grid(row=1,column=0,padx=padxy,pady=padxy)
+
+var8_1 = tk.StringVar()
+var8_1.set('頁面停留秒數')
+lab8_1 = tk.Label(fr8, textvariable = var8_1)
+lab8_1.grid(row=0,column=0,columnspan=3,padx=padxy,pady=padxy)
+
+en8_1 = tk.Entry(fr8,width=5)
+en8_1.insert(0,page_sleep_times[0])
+en8_1.grid(row=1,column=0,padx=padxy,pady=padxy)
+
+var8_2 = tk.StringVar()
+var8_2.set('-')
+lab8_2 = tk.Label(fr8, textvariable = var8_2)
+lab8_2.grid(row=1,column=1,padx=padxy,pady=padxy)
+
+en8_2 = tk.Entry(fr8,width=5)
+en8_2.insert(0,page_sleep_times[1])
+en8_2.grid(row=1,column=2,padx=padxy,pady=padxy)
+
+#frame 9
+fr9 = tk.Frame(frr)
+fr9.grid(row=2,column=0,padx=padxy,pady=padxy)
+
+var9_1 = tk.StringVar()
+var9_1.set('執行時間')
+lab9_1 = tk.Label(fr9, textvariable = var9_1)
+lab9_1.grid(row=0,column=0,columnspan=3,padx=padxy,pady=padxy)
+
+en9_1 = tk.Entry(fr9,width=5)
+en9_1.insert(0,exercise_time[0])
+en9_1.grid(row=1,column=0,padx=padxy,pady=padxy)
+
+var9_2 = tk.StringVar()
+var9_2.set('-')
+lab9_2 = tk.Label(fr9, textvariable = var9_2)
+lab9_2.grid(row=1,column=1,padx=padxy,pady=padxy)
+
+en9_2 = tk.Entry(fr9,width=5)
+en9_2.insert(0,exercise_time[1])
+en9_2.grid(row=1,column=2,padx=padxy,pady=padxy)
+
+#frame 0
+
+fr_0 = tk.Frame(frr)
+fr_0.grid(row=3,column=0,padx=padxy,pady=padxy)
+btn0_1 = tk.Button(fr_0, text = '保存並退出',command = save_and_exit)
+btn0_1.grid(row=0,column=0,padx=padxy,pady=padxy)
+btn0_2 = tk.Button(fr_0, text = '不儲存離開',command = exit)
+btn0_2.grid(row=0,column=1,padx=padxy,pady=150)
 
 #显示主窗口
 window.mainloop()
